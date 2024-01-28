ef0f01380fc9a9aa93cdb93564bde5c2c9ecfc64
enochCodes <henoksamson57@gmail.com>
2024-01-28 23:36:48 +0300

    done

--- _printf.c
+++ _printf.c
@@ -1,22 +1,66 @@
 #include "main.h"
+void handle_char(va_list args, int *count)
+{
+	char ch;
 
-/*
- * the _printf function handles the following format specifiers:
- * %c - print single character
- * %s - print string
- * %% - print the percent sign
+	ch = va_arg(args, int);
+	putchar(ch);
+	(*count)++;
+}
+void handle_string(va_list args, int *count)
+{
+	int j;
+	char *str;
+
+	str = va_arg(args, char *);
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		putchar(str[j]);
+		(*count)++;
+	}
+}
+void handle_decimal(va_list args, int *count)
+{
+	int num;
+	int temp;
+	int divisor;
+
+	num = va_arg(args, int);
+	divisor = 1;
+	temp = num;
+
+	if (num < 0)
+	{
+		putchar('-');
+		(*count)++;
+		num = -num;
+	}
+	while (temp > 9)
+	{
+		temp /= 10;
+		divisor *= 10;
+	}
+	while (divisor > 0)
+	{
+		int digit = num / divisor;
+
+		putchar(digit + '0');
+		(*count)++;
+		num %= divisor;
+		divisor /= 10;
+	}
+}
+/**
+ * _printf - function handles the following format specifiers:
  */
-
 int _printf(const char *format, ...)
 {
-	int count;
-	
-	count = 0;
-	
+	int count, i;
 	va_list args;
+
+	count = 0;
 	va_start(args, format);
-	int i;
-	
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -26,40 +70,30 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			if (format[i + 1] == 'c')
+			switch (format[i + 1])
 			{
-				char ch = va_arg(args, int);
-				putchar(ch);
-				count++;
-				i++;
-			}
-			else if (format[i + 1] == 's')
-			{
-				char *str = va_arg(args, char *);
-				int j;
-				
-				for (j = 0; str[j] != '\0'; j++)
-				{
-					putchar(str[j]);
+				case 'c':
+					handle_char(args, &count);
+					break;
+				case 's':
+					handle_string(args, &count);
+					break;
+				case 'd':
+					handle_decimal(args, &count);
+					break;
+				case '%':
+					putchar('%');
 					count++;
-				}
-				i++;
-			}
-			else if (format[i + 1] == '%')
-			{
-				putchar('%');
-				count++;
-				i++;
-			}
-			else
-			{
+					break;
+				default:
 				putchar('%');
 				putchar(format[i + 1]);
 				count += 2;
-				i++;
+				break;
 			}
+			i++;
 		}
 	}
 	va_end(args);
-	return count;
+	return (count);
 }
---
+++ test.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void)
+{
+    _printf("Hello, %s!\n", "world");
+    _printf("hello, %d + %d\n", 12, 12);
+    _printf("sgin, %%\n"); 
+
+    return 0;
+}
