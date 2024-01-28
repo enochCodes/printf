fc9381e6267e5c820b8ebf4cd254d9c5e82b99ef
enochCodes <henoksamson57@gmail.com>
2024-01-29 01:27:00 +0300

    done_fix

--- main.h
+++ main.h
@@ -6,8 +6,5 @@
 
 int _printf(const char *format, ...);
 void handle_integer(va_list args, int *count);
-void handle_decimal(va_list args, int *count);
-void handle_char(va_list args, int *count);
-void handle_string(va_list args, int *count);
 void _putchar(char c, int *count);
 #endif
