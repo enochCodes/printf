885057e2d18ce0882258db96ebb09ad1d916340f
enochCodes <henoksamson57@gmail.com>
2024-01-29 20:38:08 +0300

    done

--- _printf.c
+++ _printf.c
@@ -31,42 +31,6 @@ void handle_string(va_list args, int *count)
 	}
 }
 /**
- * handle_decimal - Handles the 'd' conversion specifier in _printf
- * @args: va_list containing the arguments
- * @count: pointer to the count of characters printed
- */
-void handle_decimal(va_list args, int *count)
-{
-	int num;
-	int temp;
-	int divisor;
-
-	num = va_arg(args, int);
-	divisor = 1;
-	temp = num;
-
-	if (num < 0)
-	{
-		putchar('-');
-		(*count)++;
-		num = -num;
-	}
-	while (temp > 9)
-	{
-		temp /= 10;
-		divisor *= 10;
-	}
-	while (divisor > 0)
-	{
-		int digit = num / divisor;
-
-		putchar(digit + '0');
-		(*count)++;
-		num %= divisor;
-		divisor /= 10;
-	}
-}
-/**
  * _printf - Custom printf function with limited functionality
  * @format: Format string containing conversion specifiers
  * Return: Number of characters printed (excluding the null byte)
@@ -94,9 +58,6 @@ int _printf(const char *format, ...)
 					break;
 				case 's':
 					handle_string(args, &count);
-					break;
-				case 'd':
-					handle_decimal(args, &count);
 					break;
 				case '%':
 					putchar('%');
