2eea6281da33814fb25079059bd3b428c117cd2b
enochCodes <henoksamson57@gmail.com>
2024-01-30 21:54:26 +0300

    done

--- _printf.c
+++ _printf.c
@@ -6,11 +6,27 @@
  */
 int _printf(const char *format, ...)
 {
-	int count, i;
+	int count = 0;
 	va_list args;
 
-	count = 0;
 	va_start(args, format);
+	count = process_format(format, args);
+	va_end(args);
+
+	return (count);
+}
+
+/**
+ * process_format - Process the format string and handle conversions
+ * @format: Format string containing conversion specifiers
+ * @args: Variable arguments list
+ * Return: Number of characters printed (excluding the null byte)
+ */
+int process_format(const char *format, va_list args)
+{
+	int count, i;
+
+	count = 0
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -20,33 +36,39 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			switch (format[i + 1])
-			{
-				case 'c':
-					handle_char(args, &count);
-					break;
-				case 's':
-					handle_string(args, &count);
-					break;
-				case '%':
-					_putchar(format[i]);
-					count++;
-					break;
-				case 'd':
-					handle_decimal(args);
-					break;
-				case 'i':
-					handle_integer(args);
-					break;
-				default:
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-					count += 2;
-					break;
-			}
+			count += handle_conversion(format[i + 1], args);
 			i++;
 		}
 	}
-	va_end(args);
 	return (count);
 }
+
+/**
+ * handle_conversion - Handle a specific conversion specifier
+ * @specifier: The conversion specifier
+ * @args: Variable arguments list
+ * Return: Number of characters printed (excluding the null byte)
+ */
+int handle_conversion(char specifier, va_list args)
+{
+	switch (format[i + 1])
+	{
+		case 'c':
+			handle_char(args);
+			return (1);
+		case 's':
+			return (handle_string(args));
+		case '%':
+			_putchar('%');
+			return (1);
+		case 'd':
+			handle_decimal(args);
+		case 'i':
+			handle_integer(args);
+		default:
+			_putchar('%');
+			_putchar(specifier);
+			return (2);
+	}
+}
+
